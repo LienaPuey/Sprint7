7034dc1835ee6d5bb21ba47cc4cd1abf3cc62c52
unknown <lienapuey@gmail.com>
2023-02-08 17:09:57 +0100

    ejercicio 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +8,33 @@ import { NgForm } from '@angular/forms';
 })
 export class HomeComponent {
 
+  budgetForm = new FormGroup({
+    web : new FormControl(false),
+    seo: new FormControl(false),
+    google: new FormControl(false)
+  }); 
 
+  webPrice = 500;
+  seoPrice = 300;
+  googlePrice = 200;
+  price:number = 0;
+  get webValue(){
+    return this.budgetForm.get('web')?.value ? this.webPrice : 0;
+  }
+
+  get seoValue(){
+    return this.budgetForm.get('seo')?.value ? this.seoPrice : 0; 
+  }
+
+  get googleValue(){
+    return this.budgetForm.get('google')?.value ? this.googlePrice : 0;
+  }
+
+  updatePrice(checkbox: string){
+    this.price = this.webValue + this.seoValue + this.googleValue;
+  }
+
+  constructor(){
+
+  }
 }
